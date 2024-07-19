60d9eb5822677f3596eb62b20bb61177a92f2e0f
YonatanHoizer <yonatanhoizer2004@gmail.com>
2024-07-19 17:18:08 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            ImageProcessor frame = new ImageProcessor();
+            frame.setVisible(true);
+        });
+    }
 }
