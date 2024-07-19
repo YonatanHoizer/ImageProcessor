3f0b642a8f5fb630bedbd1eb8152602029005f71
YonatanHoizer <yonatanhoizer2004@gmail.com>
2024-07-19 17:22:04 +0300

    EliminateRedManipulation.java

--- EliminateRedManipulation.java
+++ EliminateRedManipulation.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class EliminateRedManipulation {
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+
+public class EliminateRedManipulation {
+
+    public BufferedImage EliminateRedFilter(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        BufferedImage imageN = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color color = new Color(image.getRGB(x, y));
+                int green = color.getGreen();
+                int blue = color.getBlue();
+
+                int rgb = new Color(0, green, blue).getRGB();
+                imageN.setRGB(x, y, rgb);
+            }
+        }
+
+        return imageN;
+    }
 }
