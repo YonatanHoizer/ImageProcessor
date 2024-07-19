53626784829be5d9cf472ffb21ea31bf29e8bc9b
YonatanHoizer <yonatanhoizer2004@gmail.com>
2024-07-19 17:23:39 +0300

    MirrorManipulation.java

--- MirrorManipulation.java
+++ MirrorManipulation.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MirrorManipulation {
+import java.awt.image.BufferedImage;
+
+public class MirrorManipulation {
+
+    public BufferedImage MirrorFilter(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        BufferedImage imageN = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                imageN.setRGB(width - 1 - x, y, image.getRGB(x, y));
+            }
+        }
+
+        return imageN;
+    }
 }
